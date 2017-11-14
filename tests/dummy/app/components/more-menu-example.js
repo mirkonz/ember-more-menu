7ba20f52df26ae8dfa0076fc7d6dbc272656039e
Mirko May <mail@mirkomay.com>
2017-11-14 22:10:46 +1300

    readme and package

--- tests/dummy/app/components/more-menu-example.js
+++ tests/dummy/app/components/more-menu-example.js
@@ -1,46 +1,43 @@
 import Ember from 'ember';
 
-const { Component, ArrayProxy, run: { schedule } } = Ember;
+const { Component, run: { schedule } } = Ember;
 
 export default Component.extend({
-  items: ArrayProxy.create({
-    content: Ember.A([
-      {
-        text: "Action 1",
-        linkTo: "index"
-      }, {
-        text: "Action 2",
-        linkTo: "index"
-      }, {
-        text: "Action 3",
-        linkTo: "index"
-      }, {
-        text: "Action 4"
-      }, {
-        text: "Action 5"
-      }, {
-        text: "Action 6",
-        href: "#"
-      }, {
-        text: "Action 7",
-        href: "#"
-      }, {
-        text: "Action 8",
-        href: "#"
-      }, {
-        text: "Action 9",
-        href: "#"
-      }])
-    }),
-    didInsertElement() {
-      schedule('afterRender', function() {
-        $('.example1').animate({'width': '50%'}, 3000);
-        $('.resizeable').resizable({
-          handles: "e",
-          start: (event, ui) => {
-            $(ui.element).trigger('hideMoreMenu');
-          }
-        });
+  items: [{
+    text: "Action 1",
+    linkTo: "index"
+  }, {
+    text: "Action 2",
+    linkTo: "index"
+  }, {
+    text: "Action 3",
+    linkTo: "index"
+  }, {
+    text: "Action 4",
+    href: "#"
+  }, {
+    text: "Action 5",
+    href: "#"
+  }, {
+    text: "Action 6",
+    href: "#"
+  }, {
+    text: "Action 7",
+    href: "#"
+  }, {
+    text: "Action 8"
+  }, {
+    text: "Action 9"
+  }],
+  didInsertElement() {
+    schedule('afterRender', function() {
+      $('.example1').animate({'width': '50%'}, 3000);
+      $('.resizeable').resizable({
+        handles: "e",
+        start: (event, ui) => {
+          $(ui.element).trigger('hideMoreMenu');
+        }
       });
-    }
+    });
+  }
 });
